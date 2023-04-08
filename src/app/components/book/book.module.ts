a7200789c943bacad6fc86f10768e1ae4811f7dc
Habiba Hamed <bibahamed99@gmail.com>
2023-04-08 13:13:11 +0200

    profile page👀

--- src/app/components/book/book.module.ts
+++ src/app/components/book/book.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { ViewBooksComponent } from './view-books/view-books.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BookFilterComponent } from './book-filter/book-filter.component';
 
 const routes: Routes = [
   {path:'books',component:ViewBooksComponent},
   {path:'book/details/:id',component:BookDetailsComponent},
-
+  {path:'book/filter',component:BookFilterComponent},
 
   ];
 
 @NgModule({
   declarations: [
     ViewBooksComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
+    BookFilterComponent
   ],
   imports: [
     CommonModule,
