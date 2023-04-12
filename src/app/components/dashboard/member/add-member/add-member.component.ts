df617e012814b8caaf8125f1ea7923bd61b76244
YasmineEl-shahat <yasmenelshahat754@gmail.com>
2023-04-12 17:23:14 +0200

    add member

--- src/app/components/dashboard/member/add-member/add-member.component.ts
+++ src/app/components/dashboard/member/add-member/add-member.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+// import { Member } from 'src/app/models/member';
+import { MemberService } from 'src/app/services/member.service';
 @Component({
   selector: 'app-add-member',
   templateUrl: './add-member.component.html',
-  styleUrls: ['./add-member.component.css']
+  styleUrls: ['./add-member.component.css'],
 })
 export class AddMemberComponent {
+  addForm: FormGroup = new FormGroup({});
+  constructor(
+    private fb: FormBuilder,
+    private _memberService: MemberService,
+    private router: Router
+  ) {}
+  errMsg: any = '';
+  ngOnInit(): void {
+    this.addForm = this.fb.group({
+      name: ['', Validators.required],
+      birthdate: [
+        '',
+        Validators.required,
+        Validators.pattern(/^\d{4}-\d{2}-\d{2}$/),
+      ],
+      hiredate: [
+        '',
+        Validators.required,
+        Validators.pattern(/^\d{4}-\d{2}-\d{2}$/),
+      ],
+      salary: new FormControl('', [Validators.required, Validators.min(1000)]),
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(11),
+          Validators.maxLength(11),
+        ],
+      ],
+      city: ['', Validators.required],
+      street: ['', Validators.required],
+      building: ['', Validators.required],
+    });
+  }
 
+  //**********form validation functions*******
+
+  isValidControl(name: string): boolean {
+    return this.addForm.controls[name].valid;
+  }
+
+  isInValidAndTouched(name: string): boolean {
+    return (
+      this.addForm.controls[name].invalid &&
+      (this.addForm.controls[name].dirty || this.addForm.controls[name].touched)
+    );
+  }
+
+  isControlHasError(name: string, error: string): boolean {
+    return (
+      this.addForm.controls[name].invalid &&
+      this.addForm.controls[name].errors?.[error]
+    );
+  }
+  //*********End of form validation functions**********
+  async onSubmit() {
+    // const member: Member = new Member();
+    const member: any = {
+      name: this.addForm.value.name,
+      birthdate: this.addForm.value.birthdate,
+      hiredate: this.addForm.value.hiredate,
+      salary: this.addForm.value.salary,
+      email: this.addForm.value.email,
+      phoneNumber: this.addForm.value.phoneNumber,
+      city: this.addForm.value.city,
+      street: this.addForm.value.street,
+      building: this.addForm.value.building,
+    };
+
+    const response = await this._memberService.post(member).subscribe(
+      async (response: any) => {
+        this.router.navigateByUrl('/dashboard/member/member');
+      },
+      (error: any) => {
+        this.errMsg = error.error.message;
+      }
+    );
+  }
 }
--- src/app/services/member.service.ts
+++ src/app/services/member.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
-import {Member } from 'src/app/models/member';
+import { Member } from 'src/app/models/member';
 
 import { HttpClient } from '@angular/common/http';
 // import { Observable } from 'rxjs';
@@ -8,11 +8,9 @@ import { Observable } from 'rxjs';
 // import { HttpClient } from '@angular/common/http';
 // import { Member } from '../models/member';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MemberService {
-
-
   // private baseUrl = 'http://localhost:8080';
 
   // constructor(public http: HttpClient) { }
@@ -22,34 +20,23 @@ export class MemberService {
   //   return this.http.get<Member[]>(url);
   // }
 
-
-
-
-
-
-
-  constructor(private _apiService: ApiService) { }
+  constructor(private _apiService: ApiService) {}
 
   getAll() {
     return this._apiService.get(`members`);
   }
 
-  getById(id:any) {
+  getById(id: any) {
     console.log(id);
     return this._apiService.getById(`members/`, id);
   }
-  // add(member) {
-  //   return this._apiService.post(`members`, member);
-  // }
-  // update(id:any, member:any): Observable<any> {
-  //   return this._apiService.patch(`members/${id}`, member);
-  // }
-  // delete(id) {
-  //   return this._apiService.delete(`members/${id}`);
-  // }
-  // deleteMember(id: number) {
-  //   return this._apiService.delete(`members/${id}`);
-  // }
+  post(member: any) {
+    return this._apiService.post(`members`, member);
+  }
+  update(id: any, member: any): Observable<any> {
+    return this._apiService.patch(`members/${id}`, member);
+  }
+
   deleteMember(id: number): Observable<any> {
     return this._apiService.delete(`members/${id}`);
   }
@@ -59,9 +46,8 @@ export class MemberService {
   }
 
   getreadingBooks(id: number): Observable<any> {
-    return this._apiService.getBook(`readingBooks/list?id=`,id);
+    return this._apiService.getBook(`readingBooks/list?id=`, id);
   }
-
 
   updateProfile(id: number, body: any) {
     return this._apiService.patch(`members/${id}`, body);
