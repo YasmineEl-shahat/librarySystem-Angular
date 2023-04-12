4abf1e496a282b7ef7d538f849bf0cf7faff6f44
Ahmed Ramadan <ahmedromio233@gmail.com>
2023-04-13 00:55:59 +0200

    function search

--- src/app/components/dashboard/member/member.component.ts
+++ src/app/components/dashboard/member/member.component.ts
@@ -10,8 +10,7 @@ import { Member } from 'src/app/models/member';
 export class MemberComponent implements OnInit {
 
   memberObj: Member[] = [];
-  searchTerm: string = '';
-
+  searchTerm: string[] = [];
   constructor(public memberService: MemberService) {}
 
   ngOnInit() {
@@ -36,30 +35,30 @@ export class MemberComponent implements OnInit {
   }
 
 
-  // search(): void {
-  //   if (this.searchTerm.length > 0) {
-  //     this.memberService.memberSearch(this.searchTerm).subscribe((response: any) => {
-  //       this.memberObj = response.data;
-  //       alert(this.memberObj);
-  //     });
-  //   } else {
-  //     this.getMembers();
-  //   }
-  // }
+  addToSearchTerm(searchTerm: string): void {
+    this.searchTerm.push(searchTerm);
+    this.search();
 
+  }
 
+  search(): void {
 
-  search(event: Event): void  {
-    const inputElement = event.target as HTMLInputElement;
-    if (inputElement && inputElement.value.length > 0) {
-      const searchTerm = inputElement.value;
-      this.memberService.memberSearch(searchTerm).subscribe((response: any) => {
-        this.memberObj = response.data;
+    console.log('searchTerm:', this.searchTerm);
+    if (this.searchTerm.length > 0) {
+      this.memberService.getAll().subscribe((members: any) => {
+        this.memberObj = members.data.filter((member: Member) =>
+          member.email?.toLowerCase().includes(this.searchTerm[0].toLowerCase())
+
+        );
+        this.searchTerm = [];
       });
     } else {
       this.getMembers();
     }
   }
+
+
+
 
 
 }
@@ -69,18 +68,14 @@ export class MemberComponent implements OnInit {
 
 
 
-
-
-
-  // deleteMember(id: number | undefined, e: any) {
-  //   e.preventDefault();
-  //   if (id) {
-  //     const confirmed = confirm(`Are you sure you want to delete member with id ${id}?`);
-  //     if (confirmed) {
-  //       this.memberService.deleteMember(id).subscribe((response: any) => {
-  //         alert(`Deleted member with id ${id} ${response.data} `);
-  //       });
-  //     }
+  // search(): void {
+  //   if (this.searchTerm.length > 0) {
+  //     this.memberService.memberSearch(this.searchTerm).subscribe((response: any) => {
+  //       this.memberObj = response.data;
+  //       alert(this.memberObj);
+  //     });
+  //   } else {
+  //     this.getMembers();
   //   }
   // }
 
