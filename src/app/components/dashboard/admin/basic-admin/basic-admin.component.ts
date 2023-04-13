79993e0761ee6092e5af7ed5be574025dcceffd6
YasmineEl-shahat <yasmenelshahat754@gmail.com>
2023-04-13 08:38:53 +0200

    get basic admin

--- src/app/components/dashboard/admin/basic-admin/basic-admin.component.ts
+++ src/app/components/dashboard/admin/basic-admin/basic-admin.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminRequest } from 'src/app/models/admin';
+import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-basic-admin',
   templateUrl: './basic-admin.component.html',
-  styleUrls: ['./basic-admin.component.css']
+  styleUrls: ['./basic-admin.component.css'],
 })
 export class BasicAdminComponent {
+  admins: AdminRequest[] = [];
 
+  constructor(public _adminService: AdminService) {}
+
+  ngOnInit() {
+    this._adminService.getAll().subscribe((response: any) => {
+      response.data.forEach((element: AdminRequest) => {
+        if (element.image)
+          element.image = `http:\\\\localhost:8080\images${
+            element.image.split('images')[1]
+          }`;
+      });
+      this.admins = response.data.filter((admin: AdminRequest) => {
+        return admin.isBase;
+      });
+    });
+  }
+
+  deleteAdmin(id: number | undefined) {
+    if (id) {
+      this._adminService.delete(id).subscribe((response: any) => {
+        this._adminService.getAll().subscribe((response: any) => {
+          this.admins = response.data;
+        });
+      });
+    }
+  }
 }
