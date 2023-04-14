1c153256e36f0b6af3d37ae13d914b7eb15fad72
Habiba Hamed <bibahamed99@gmail.com>
2023-04-14 21:37:07 +0200

    Add Book📔

--- src/app/models/book.ts
+++ src/app/models/book.ts
@@ -1,17 +1,35 @@
-export class Book{
-    _id:number=0;
-    title:string='';
-    auther:string='';
-    publisher:string='';
-    publishingDate:string='';
-    category:string='';
-    edition:number=0;
-    pages:number=0;
-    avilable:number=0;
-    shelfNo:number=0;
-    numOfCopies:number=0;
-    image:string='';
-    noOfBorrowing:number=0;
-    noOfReading:number=0;
-    createdAt:string=''
+export class Book {
+  _id: number = 0;
+  title: string = '';
+  auther: string = '';
+  publisher: string = '';
+  publishingDate: string = '';
+  category: string = '';
+  edition: number = 0;
+  pages: number = 0;
+  avilable: number = 0;
+  shelfNo: number = 0;
+  numOfCopies: number = 0;
+  image: string = '';
+  noOfBorrowing: number = 0;
+  noOfReading: number = 0;
+  createdAt: string = '';
+}
+
+export interface BookRequest {
+  _id?: number;
+  title?: string;
+  auther?: string;
+  publisher?: string;
+  publishingDate?: string;
+  category?: string;
+  edition?: number;
+  pages?: number;
+  avilable?: number;
+  shelfNo?: number;
+  numOfCopies?: number;
+  image?: string;
+  noOfBorrowing?: number;
+  noOfReading?: number;
+  createdAt?: Date;
 }
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -2,30 +2,32 @@ import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
-
-  constructor(private _apiService:ApiService) { }
+  constructor(private _apiService: ApiService) {}
 
   get() {
-    return this._apiService.get(`books`)
-
-   }
-  getLimit() {
-    return this._apiService.get(`getCountBook`)
-
-   }
-
-  getLatestBooks(){
-    return this._apiService.get(`newBooks`)
+    return this._apiService.get(`books`);
   }
-  getBook(id: string){
+
+  getLimit() {
+    return this._apiService.get(`getCountBook`);
+  }
+
+  getLatestBooks() {
+    return this._apiService.get(`newBooks`);
+  }
+
+  getBook(id: string) {
     return this._apiService.get(`books/${id}`);
   }
 
-  searchBook(formData: any){
-    return this._apiService.post(`bookSearchFilter`,formData);
+  searchBook(formData: any) {
+    return this._apiService.post(`bookSearchFilter`, formData);
   }
 
+  addBook(data: any) {
+    return this._apiService.post(`books`, data);
+  }
 }
